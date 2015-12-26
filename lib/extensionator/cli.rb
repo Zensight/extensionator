5e2736c407dabe26f14cf0776519b32d42fb70ac
Patrik Ragnarsson <patrik@starkast.net>
2015-12-26 01:01:28 +0100

    Improve README and CLI documentation

--- lib/extensionator/cli.rb
+++ lib/extensionator/cli.rb
@@ -26,9 +26,9 @@ module Extensionator
       Slop.parse do |o|
         o.string "-d", "--directory", "Directory containing the extension. (Default: .)"
         o.string "-i", "--identity", "Location of the pem file to sign with."
-        o.string "-o", "--output", "Location of the output file. (Default: 'extension.[zip|crx]')"
+        o.string "-o", "--output", "Location of the output file. (Default: 'output[.zip|.crx|]')"
         o.string "-e", "--exclude", "Regular expression for filenames to exclude. (Default: \.crx$)"
-        o.string "-f", "--format", "Type of file to produce, either zip or crx. (Default: crx)"
+        o.string "-f", "--format", "Type of file to produce, either 'zip', 'dir' or 'crx'. (Default: crx)"
         o.string "--inject-version", "Inject a version number into the manifest file. (Default: none)"
         o.bool "--inject-key", "Inject a key parameter into the manifest file. (Default: no)"
         o.bool "--strip-key", "Remove the key parameter from the manifest file. (Default: no)"
